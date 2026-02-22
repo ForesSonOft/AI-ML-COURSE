0dd54f6f7ec244124be78670ca2155fd45a64e02
ragurm <sragu251996@gmail.com>
2026-02-22 19:09:26 +0530

    first commit

---
+++ personalinfo.py
@@ -0,0 +1,20 @@
+#fgSbllaron Suresn to Everyone
+
+# personal_info.py
+
+# A program that collects and saves user information
+
+# Get information from user
+print(" === Personal InfoLogger === ")
+name = input("Enter your name:")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+
+# Create a formatted message
+info = f"Name: {name}\nAge:{age}\nCity: {city}\n"
+
+# Save to file
+file = open("user_info.txt", "w")
+file.write(info)
+file.close()
+print("nv Information saved touser info.txt")
